6be1ebf3756231f23456df01fbbbfea94d99a53e
liyeon <fldus612@gmail.com>
2023-03-13 18:12:03 +0900

    📒 Day9 Exercise - 자바의 정석 스트링 클래스의 메소드를 이용한 예제 풀이

---
+++ Day09_0313/src/exercise/Ex9_3.java
@@ -0,0 +1,21 @@
+package exercise;
+
+public class Ex9_3 {
+	public static void main(String[] args) {
+		String fullPath = "c:\\jdk1.5\\work\\PathSeparateTest.java";
+		String path = "";
+		String fileName = "";
+		/*
+		 * (1) 알맞은 코드를 넣어 완성하시오.
+		 */
+		int idx = fullPath.lastIndexOf("\\");
+		
+		path = fullPath.substring(0,idx+1);
+		fileName = fullPath.substring(idx+1);
+		
+		
+		System.out.println("fullPath:" + fullPath);
+		System.out.println("path:" + path);
+		System.out.println("fileName:" + fileName);
+	}
+}
